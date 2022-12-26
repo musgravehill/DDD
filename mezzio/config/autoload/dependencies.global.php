a8b7eab4f9411f320eea3a882198a847befc69cd
bobubuntu <musgravehill@gmail.com>
2022-12-26 18:58:23 +0300

    storageless: session in JWT

--- mezzio/config/autoload/dependencies.global.php
+++ mezzio/config/autoload/dependencies.global.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types=1);  
 
 return [
     // Provides application-wide services.
@@ -23,6 +23,8 @@ return [
             // Fully\Qualified\ClassName::class => Fully\Qualified\FactoryName::class,
             ErrorHandler::class => Mezzio\Container\ErrorHandlerFactory::class,
             Mezzio\Middleware\ErrorResponseGenerator::class => Mezzio\Container\ErrorResponseGeneratorFactory::class,
+            //
+            'SessionMiddleware' => App\Middleware\SessionMiddlewareFactory::class,
         ],
     ],
 ];
---
+++ mezzio/src/App/src/Middleware/SessionMiddlewareFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use Dflydev\FigCookies\Modifier\SameSite;
+use Dflydev\FigCookies\SetCookie;
+use Lcobucci\Clock\SystemClock;
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Signer;
+use Lcobucci\JWT\Signer\Key\InMemory;
+use PSR7Sessions\Storageless\Http\SessionMiddleware;
+//
+use Psr\Container\ContainerInterface;
+
+class SessionMiddlewareFactory
+{
+    public function __invoke(ContainerInterface $container)
+    {
+        $key = 'mBC5v1sOKVvbdEitdSBenu59nfNfhwkedkJVNabosTw=';
+
+        //return SessionMiddleware::fromSymmetricKeyDefaults(\Lcobucci\JWT\Signer\Key\InMemory::plainText($key),  60);
+
+        return new SessionMiddleware(
+            Configuration::forSymmetricSigner(
+                new Signer\Hmac\Sha256(),
+                InMemory::plainText($key)
+            ),
+            // Override the default `__Secure-slsession` which only works on HTTPS
+            SetCookie::create('slsession')
+                // Disable mandatory HTTPS
+                ->withSecure(false)
+                ->withHttpOnly(false)
+                ->withSameSite(SameSite::strict())
+                ->withPath('/'),
+            60, // session idle timeout, in seconds
+            SystemClock::fromUTC(),
+        );
+    }
+}
