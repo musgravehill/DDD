2d92afb67e7088b55ab1f343ec6a62331bee4caf
EvaPC <mdeed87@gmail.com>
2023-03-23 18:45:42 +0300

    Add Entities

---
+++ mezzio/src/DoctrineORM/src/Entity/Goal.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineORM\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\JoinTable;
+use Doctrine\ORM\Mapping\ManyToMany;
+use Doctrine\ORM\Mapping\ManyToOne;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'goal')]
+class Goal
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', name: 'ttl', length: 128)]
+    private string $ttl;
+
+    /** Many interests have one user */
+    /** Owner side (has ForeignKey FK) */
+    #[ManyToOne(targetEntity: User::class, inversedBy: 'goals')] // inversedBy: User->goals
+    #[JoinColumn(name: 'user_id', referencedColumnName: 'id')] // goal.user_id   user.id 
+    private User|null $user = null;
+     
+}
--- mezzio/src/DoctrineORM/src/Entity/User.php
+++ mezzio/src/DoctrineORM/src/Entity/User.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace DoctrineORM\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\JoinTable;
+use Doctrine\ORM\Mapping\ManyToMany;
+use Doctrine\ORM\Mapping\OneToMany;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'user')]
@@ -13,12 +18,36 @@ class User
     #[ORM\Id]
     #[ORM\Column(type: 'integer')]
     #[ORM\GeneratedValue]
-    private ?int $id=null;
+    private ?int $id = null;
 
     #[ORM\Column(type: 'string')]
     private string $email;
- 
+
     #[ORM\Column(type: 'text', name: 'about_me', length: 65000)]
     private string $aboutMe;
 
+    /** Many users have many interests */
+    /** Owner side */
+    /** @var Collection<int, Interest> */
+    #[ManyToMany(targetEntity: Interest::class, inversedBy: 'users')]  //  inversedBy: Interest->users
+    #[JoinTable(name: 'users_groups')]
+    private Collection $interests;
+
+    /** One user has many goals */
+    /** Inverse side */
+    /** @var Collection<int, Goal> */
+    #[OneToMany(targetEntity: Goal::class, mappedBy: 'user')] // mappedBy: Goal->user
+    private Collection $goals;
+
+    public function __construct()
+    {
+        $this->interests = new ArrayCollection();
+        $this->goals = new ArrayCollection();
+    }
+
+    public function addInterest(Interest $interest): void
+    {
+        $interest->addUser($this); // synchronously updating Inverse side
+        $this->interests[] = $interest;
+    }
 }
