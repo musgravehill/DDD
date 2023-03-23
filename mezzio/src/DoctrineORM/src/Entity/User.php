6b5701e358024af82771faeea74a81f9acea4dac
EvaPC <mdeed87@gmail.com>
2023-03-23 21:51:23 +0300

    TODO

--- mezzio/src/DoctrineORM/src/Entity/User.php
+++ mezzio/src/DoctrineORM/src/Entity/User.php
@@ -7,6 +7,7 @@ namespace DoctrineORM\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\InverseJoinColumn;
 use Doctrine\ORM\Mapping\JoinColumn;
 use Doctrine\ORM\Mapping\JoinTable;
 use Doctrine\ORM\Mapping\ManyToMany;
@@ -36,21 +37,39 @@ class User
     private Collection $interests;
 
     /** OneToMany:bi One user has many goals */
-    /** Inverse side */    
+    /** Inverse side */
     /** @var Collection<int, Goal> */
     #[OneToMany(targetEntity: Goal::class, mappedBy: 'user')] // mappedBy: Goal->user
     private Collection $goals;
 
     /** ManyToOne:uni Many users have one city */
-    /** Owner side */    
+    /** Owner side */
     #[ManyToOne(targetEntity: City::class)]
     #[JoinColumn(name: 'city_id', referencedColumnName: 'id')]
     private City|null $city = null;
+
+    /** ManyToMany:self-referencing Many users have many users=friends */
+    /** @var Collection<int, User> */
+    #[JoinTable(name: 'users_friends')]
+    #[JoinColumn(name: 'user_id', referencedColumnName: 'id')]
+    #[InverseJoinColumn(name: 'friend_user_id', referencedColumnName: 'id')]
+    #[ManyToMany(targetEntity: 'User')] // , inversedBy: 'friendsWithMe'
+    private Collection $myFriends;
+
+    /** ManyToMany:self-referencing Many users have many users=friends */
+    /** @var Collection<int, User> */
+    /* #[ManyToMany(targetEntity: User::class, mappedBy: 'myFriends')]
+    private Collection $friendsWithMe;
+    */
+
+    //bi-derectional with $friendsWithMe;
 
     public function __construct()
     {
         $this->interests = new ArrayCollection();
         $this->goals = new ArrayCollection();
+        //$this->friendsWithMe = new ArrayCollection();
+        $this->myFriends = new ArrayCollection();
     }
 
     public function addInterest(Interest $interest): void
