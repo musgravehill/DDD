bab1bf9c5b94502645e945bdf2565fffbbfb7956
bobubuntu <musgravehill@gmail.com>
2023-03-27 22:35:02 +0300

    EM config

--- mezzio/src/DoctrineORM/src/Service/EntityManagerFactory.php
+++ mezzio/src/DoctrineORM/src/Service/EntityManagerFactory.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace DoctrineORM\Service;
 
 use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AttributeDriver;
 use Doctrine\ORM\ORMSetup;
-
-use Psr\Container\ContainerInterface;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
+
+use Psr\Container\ContainerInterface;
+
 
 class EntityManagerFactory
 {
@@ -24,9 +27,12 @@ class EntityManagerFactory
          *  port (integer): Port of the database to connect to.
          *  dbname (string): Name of the database/schema to connect to.
          *  unix_socket (string): Name of the socket used to connect to the database.
-         *  charset (string): The charset used when connecting to the database.
-         *
+         *  charset (string): The charset used when connecting to the database.        
          * 
+         * $paths = [dirname(__DIR__, 1) . '/Entity'];
+         * $isDevMode = false;
+         * $config = ORMSetup::createAttributeMetadataConfiguration($paths, $isDevMode);
+         *
          * If $isDevMode is true caching is done in memory with the ArrayAdapter. Proxy objects are recreated on every request. 
          *  If $isDevMode is false, check for Caches in the order APCu, Redis (127.0.0.1:6379), Memcache (127.0.0.1:11211) unless `$cache` is passed as fourth argument.
          *  If $isDevMode is false, set then proxy classes have to be explicitly created through the command line.
@@ -41,11 +47,6 @@ class EntityManagerFactory
             'driver' => 'pdo_mysql',
         ];
 
-        $paths = [dirname(__DIR__, 1) . '/Entity'];
-        $isDevMode = false;
-        $config = ORMSetup::createAttributeMetadataConfiguration($paths, $isDevMode);
-
-
         $applicationMode = 'development';
 
         if ($applicationMode == "development") {
@@ -56,7 +57,11 @@ class EntityManagerFactory
             $metadataCache = new PhpFilesAdapter('doctrine_metadata');
         }
 
+        $config = new Configuration;
+
         $config->setMetadataCache($metadataCache);
+        $driverImpl = new AttributeDriver([dirname(__DIR__, 1) . '/Entity']);
+        $config->setMetadataDriverImpl($driverImpl);
         $config->setQueryCache($queryCache);
         $config->setProxyDir(dirname(__DIR__, 1) . '/Proxy');
         $config->setProxyNamespace('DoctrineORM\Proxy');
